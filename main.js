5297679e013a90db939a8216418d9548f76ec3cc
vitalii <dumavb777@gmail.com>
2019-05-28 12:50:42 +0300

    sol

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-const tagSpan = document.querySelector('span');
-tagSpan.textContent = sessionStorage.getItem('textSpan') || 0;
-let textSpan = tagSpan.textContent;
-
-function countClick() {
-    return textSpan++;
-}
+const containerCounter = document.querySelector('span');
+containerCounter.textContent = sessionStorage.getItem('currentDigit') || 0;
 
 document.addEventListener('click', () => {
-    const newTextSpan = countClick();
-    tagSpan.textContent = newTextSpan;
-    sessionStorage.setItem('textSpan', newTextSpan);
+    containerCounter.textContent = (+containerCounter.textContent) + 1;
+    sessionStorage.setItem('currentDigit', containerCounter.textContent);
 })
